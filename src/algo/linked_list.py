743c60a591a2e6049a5a62b8ab2de92f79f4f15c
dnmca <dinamicandriy@gmail.com>
2021-06-10 13:25:07 +0300

    [algo] linked list: merge, remove problems

--- src/algo/linked_list.py
+++ src/algo/linked_list.py
@@ -9,21 +9,18 @@ class ListNode:
 def get_length(head: ListNode) -> int:
     """Get length of LinkedList"""
     count = 0
-    while True:
+    while head:
         count += 1
-        if head.next is None:
-            break
         head = head.next
     return count
 
 
-def to_list(head: ListNode):
+def to_list(head: ListNode) -> list:
     """convert to python list"""
+
     values = []
-    while True:
+    while head:
         values.append(head.val)
-        if head.next is None:
-            break
         head = head.next
     return values
 
@@ -39,10 +36,8 @@ def get_decimal_value(head: ListNode) -> int:
     Return the decimal value of the number in the linked list.
     """
     binary = ''
-    while True:
+    while head:
         binary += str(head.val)
-        if head.next is None:
-            break
         head = head.next
     return int(binary, 2)
 
@@ -61,7 +56,7 @@ def delete_node(node: ListNode):
     node.next = node.next.next
 
 
-def middle_node(head: ListNode):
+def middle_node(head: ListNode) -> ListNode:
     """
     LeetCode 876
 
@@ -77,3 +72,75 @@ def middle_node(head: ListNode):
             return head.val
         head = head.next
         idx += 1
+
+
+def merge_two_lists(l1: ListNode, l2: ListNode) -> ListNode:
+    """
+    LeetCode 21
+
+    Merge two sorted linked lists and return it as a sorted list.
+    The list should be made by splicing together the nodes of the first two lists.
+    """
+
+    head = None
+
+    while l1 or l2:
+        if l1 and l2:
+            if l1.val < l2.val:
+                value = l1.val
+                l1 = l1.next
+            else:
+                value = l2.val
+                l2 = l2.next
+        elif l1:
+            value = l1.val
+            l1 = l1.next
+        elif l2:
+            value = l2.val
+            l2 = l2.next
+
+        if head is None:
+            head = ListNode(value)
+        else:
+            curr = head
+            while curr.next is not None:
+                curr = curr.next
+            curr.next = ListNode(value)
+    return head
+
+
+def merge_two_lists_recursive(l1: ListNode, l2: ListNode) -> ListNode:
+    """
+    LeetCode 21 - Best Practice
+    """
+    if l1 is None:
+        return l2
+    elif l2 is None:
+        return l1
+    elif l1.val < l2.val:
+        l1.next = merge_two_lists_recursive(l1.next, l2)
+        return l1
+    else:
+        l2.next = merge_two_lists_recursive(l1, l2.next)
+        return l2
+
+
+def remove_elements(head: ListNode, val: int) -> ListNode:
+    """
+    LeetCode 203
+
+    Given the head of a linked list and an integer val,
+    remove all the nodes of the linked list that has
+    Node.val == val, and return the new head.
+    """
+
+    # it is possible to solve this problem with iteration
+    # (add dummy node in front and return dummy.next at the end)
+
+    if head is None:
+        return None
+    elif head.val == val:
+        return remove_elements(head.next, val)
+    else:
+        head.next = remove_elements(head.next, val)
+        return head
