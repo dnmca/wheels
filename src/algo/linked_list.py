c36a133af0760fb6930953a4feb38670a4a46afd
dnmca <dinamicandriy@gmail.com>
2021-06-14 15:09:28 +0300

    [algo] linked list: intersection, cicle & duplicates

--- src/algo/linked_list.py
+++ src/algo/linked_list.py
@@ -1,9 +1,9 @@
 
 
 class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 def get_length(head: ListNode) -> int:
@@ -23,6 +23,10 @@ def to_list(head: ListNode) -> list:
         values.append(head.val)
         head = head.next
     return values
+
+
+def to_str(head: ListNode) -> str:
+    return ' -> '.join([str(x) for x in to_list(head)])
 
 
 def get_decimal_value(head: ListNode) -> int:
@@ -144,3 +148,207 @@ def remove_elements(head: ListNode, val: int) -> ListNode:
     else:
         head.next = remove_elements(head.next, val)
         return head
+
+
+def concatenate(l1: ListNode, l2: ListNode):
+    """concatenate two lists"""
+    while l1.next:
+        l1 = l1.next
+    l1.next = l2
+
+
+def reverse_list(head: ListNode) -> ListNode:
+    """
+    LeetCode 206
+
+    Given the head of a singly linked list,
+    reverse the list, and return the reversed list.
+    """
+    if head is None:
+        return None
+    elif head.next is None:
+        return head
+    else:
+        new = reverse_list(head.next)
+        concatenate(new, head)
+        head.next = None
+        return new
+
+
+def reverse_list_iterative(head: ListNode) -> ListNode:
+    """
+    LeetCode 206 - Best Practice
+    """
+    prev = None
+    while head:
+        curr = head
+        head = head.next
+        curr.next = prev
+        prev = curr
+    return prev
+
+
+def is_palindrome(head: ListNode) -> bool:
+    """
+    LeetCode 234
+
+    Given the head of a singly linked list,
+    return true if it is a palindrome.
+    """
+    values = []
+    while head:
+        values.append(head.val)
+        head = head.next
+    return values == values[::-1]
+
+
+def is_palindrome_bp(head: ListNode) -> bool:
+    """
+    LeetCode 234 - best practice
+    O(N) time complexity, O(N/2) space complexity
+
+    Three steps:
+    1. Find the midpoint of the linked list
+    2. Push the second half values into the stack
+    3. Pop values out from stack, and compare the to the first half of the linked list
+
+    """
+
+    if not head or not head.next:
+        return True
+
+    slow = fast = cur = head
+
+    # 1. Get the midpoint (slow)
+    while fast and fast.next:
+        fast, slow = fast.next.next, slow.next
+
+    # 2. Push the second half into the stack
+    stack = [slow.val]
+    while slow.next:
+        slow = slow.next
+        stack.append(slow.val)
+
+    # 3. Compare
+    while stack:
+        if stack.pop() != cur.val:
+            return False
+        cur = cur.next
+
+    return True
+
+
+def get_intersection_node(head_a: ListNode, head_b: ListNode) -> ListNode:
+    """
+    LeetCode 160
+
+    Given the heads of two singly linked-lists headA and headB,
+    return the node at which the two lists intersect.
+    If the two linked lists have no intersection at all, return null.
+    """
+
+    # Dummy solution - Time Limit Exceeded
+
+    # while head_a:
+    #     copy_b = head_b
+    #     while copy_b:
+    #         if copy_b == head_a:
+    #             return head_a
+    #         copy_b = copy_b.next
+    #     head_a = head_a.next
+    # return None
+
+    len_a = get_length(head_a)
+    len_b = get_length(head_b)
+
+    if len_a > len_b:
+        for _ in range(len_a - len_b):
+            head_a = head_a.next
+
+    if len_b > len_a:
+        for _ in range(len_b - len_a):
+            head_b = head_b.next
+
+    while head_a and head_b:
+
+        if head_a == head_b:
+            return head_a
+
+        head_a = head_a.next
+        head_b = head_b.next
+
+    return None
+
+
+def get_intersection_node_bp(head_a: ListNode, head_b: ListNode) -> ListNode:
+    """
+    LeetCode 160 - best practice
+    """
+    a, b = head_a, head_b
+    while a != b:
+        a = a.next if a else head_a
+        b = b.next if b else head_b
+    return a
+    # only 2 ways to get out of the loop,
+    # they meet or the both hit the end=None
+
+
+def has_cycle(head: ListNode) -> bool:
+    """
+    LeetCode - 141
+
+    Given head, the head of a linked list,
+    determine if the linked list has a cycle in it.
+
+    There is a cycle in a linked list if there is some node
+    in the list that can be reached again by continuously
+    following the next pointer. Internally, pos is used to
+    denote the index of the node that tail's next pointer
+    is connected to. Note that pos is not passed as a parameter.
+
+    Return true if there is a cycle in the linked list. Otherwise, return false.
+    """
+    occurrence = set()
+    while head:
+        if head in occurrence:
+            return True
+        else:
+            occurrence.add(head)
+        head = head.next
+    return False
+
+
+def has_cycle_bp(head: ListNode) -> bool:
+    """
+    Tortoise and hare approach - best practice
+
+    Uses O(1) memory
+
+    https://en.wikipedia.org/wiki/Cycle_detection#Tortoise_and_hare
+    """
+    try:
+        slow = head
+        fast = head
+        while slow is not fast:
+            slow = slow.next
+            fast = fast.next.next
+        return True
+    except:
+        return False
+
+
+def delete_duplicates(head: ListNode) -> ListNode:
+    """
+    LeetCode - 83
+
+    Given the head of a sorted linked list, delete all duplicates
+    such that each element appears only once. Return the linked list sorted as well.
+    """
+    copy = head
+    while copy and copy.next:
+        if copy.val == copy.next.val:
+            delete_node(copy)              # remove first duplicate (among 2)
+            # copy.next = copy.next.next   # remove second duplicate (among 2) - BP
+        else:
+            copy = copy.next
+    return head
